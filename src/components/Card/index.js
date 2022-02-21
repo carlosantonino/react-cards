4e0748da198760293821fcd6207022a31d4e347f
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-02-21 16:37:49 -0300

    Criando Card

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Card extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <h1>Isso é um teste</h1>
+            </>
+        );
+    }
+}
+ 
+export default Card;
