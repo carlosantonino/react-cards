25c9b3f598f37cde238976b4b8c53494c55016ac
Carlos <72629502+carlosantonino@users.noreply.github.com>
2022-10-23 18:49:16 -0300

    ajuste no titulo do card

--- src/Cards/CardUI.jsx
+++ src/Cards/CardUI.jsx
@@ -8,7 +8,7 @@ const Card = props => {
                 <img src={props.imgsrc} alt="image 1" className='card-img-top' />
             </div>
             <div className="card-body text-dark">
-                <h4 className="card-title">Card Title</h4>
+                <h4 className="card-title">Titulo do Card</h4>
                 <p className="card-text text-secondary">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam numquam quis quibusdam, quasi optio recusandae eligendi provident neque quisquam voluptas.
                     <a href="#" className='btn btn-outline-success'>Saiba mais</a>
