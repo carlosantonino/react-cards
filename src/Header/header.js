b630ee0d1509ad499415a63ccc36e44a8b74b523
Carlos <72629502+carlosantonino@users.noreply.github.com>
2022-10-23 10:34:02 -0300

    Cabeçalho

---
+++ src/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css'
+
+export default function Header() {
+    return(
+        <header>
+                <h1>Logo</h1>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+        </header>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Card from './Cards/Cards';
+import App from './App';
 
 ReactDOM.render(
-  <Card />,
+  <App />,
   document.getElementById('root')
 );
