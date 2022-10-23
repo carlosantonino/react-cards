43b0a67b3b29f41f4d776eb773d1c869e6077032
Carlos <72629502+carlosantonino@users.noreply.github.com>
2022-10-23 18:37:53 -0300

    criação de titulo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Cards from './Cards/Cards';
 import Footer from './Footer/footer';
 import Header from './Header/header';
+import Title from './Title/title';
 
 
 export default function App() {
   return(
     <div>
       <Header />
+      <Title />
       <Cards />
       <Footer />
     </div>
---
+++ src/Title/title.js
@@ -0,0 +1,9 @@
+import React from "react"
+import './title.css';
+export default function Title() {
+    return(
+        <div>
+            <h1 id="titulo">Animais de estimação</h1>
+        </div>
+    )
+}
