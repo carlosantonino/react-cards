e880db756e1c9317fa6dac32a6ece15e5d6ff7b5
Carlos <72629502+carlosantonino@users.noreply.github.com>
2022-10-23 17:25:40 -0300

    criação do rodape

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Cards from './Cards/Cards';
+import Footer from './Footer/footer';
 import Header from './Header/header';
+
 
 export default function App() {
   return(
     <div>
       <Header />
       <Cards />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './footer.css';
+export default function Footer() {
+    return (
+        <footer>
+            <p>Todos os direitos reservados &copy; 2022.</p>
+        </footer>
+    )
+}
